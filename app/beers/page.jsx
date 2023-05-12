77aa1c86a1f0fd44259a247a1ba1a5a44be54088
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-12 11:29:33 -0400

    added new page

---
+++ app/beers/page.jsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <main className="p-8">
+            <h1 className="text-emerald-400 text-9xl font-black">The Beers</h1>
+        </main>
+    )
+}
